58b67f697f840136c4d602a7b9550623ea35c387
iturres <emanueliturres.co@gmail.com>
2023-11-01 20:54:48 -0300

    Feat: Enhance decoder by adding 'decode_word' method and 'decode' invocation

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -26,3 +26,16 @@ MORSE_TO_LETTER = {
   '-.--' => 'Y',
   '--..' => 'Z'
 }.freeze
+
+# decode_char
+
+# *decode_char will expect a block to be passed in (using --yield-- or --&block--)
+# * choose the one that makes the most sense for you!.
+# !delete the comments above when done :thumbs_up:.
+def decode_word(word)
+  word.split(/\s{1}/).map { |char| decode_char(char) { |letter| letter } }.join
+end
+
+# decode
+
+p decode('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
